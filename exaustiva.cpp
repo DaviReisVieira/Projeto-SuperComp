46c0f7eb48ce8dcde161c3ffb1c81fc32be3ad3a
DaviReisVieira <vieira08davi38gmail.com>
2023-05-25 01:58:09 -0300

    Project being done 50%

--- exaustiva.cpp
+++ exaustiva.cpp
@@ -10,7 +10,8 @@
 
 using namespace std;
 
-struct Filme {
+struct Filme
+{
     int id;
     int inicio;
     int fim;
@@ -18,53 +19,49 @@ struct Filme {
     std::bitset<24> horario;
 };
 
-struct Categoria {
+struct Categoria
+{
     int id;
     int quantidade;
 };
 
-struct Maratona {
+struct Maratona
+{
     std::bitset<24> disponibilidade;
     std::vector<Filme> filmes;
 };
 
-std::bitset<24> gera_horario(int inicio, int fim) {
+std::bitset<24> gera_horario(int inicio, int fim)
+{
     std::bitset<24> horario;
 
-    if (inicio == fim) {
+    if (inicio == fim)
+    {
         horario.set(inicio);
         return horario;
     }
-    
-    for (int i = inicio; i < fim; i++) {
+
+    for (int i = inicio; i < fim; i++)
+    {
         horario.set(i);
     }
 
     return horario;
 }
 
-void preenche_bitset(std::bitset<24> &horarios_disponiveis, int inicio, int fim) {
-    if (inicio == fim) {
-        horarios_disponiveis.set(inicio);
-        return;
-    }
-
-    for (int i = inicio; i < fim; i++) {
-        horarios_disponiveis.set(i);
-    }
-}
-
-void busca_exaustiva(vector<Filme>& filmes, vector<Categoria>& categorias, Maratona& maratona) {
+void busca_exaustiva(vector<Filme> &filmes, vector<Categoria> &categorias, Maratona &maratona)
+{
     int maximo = 0;
     int size_of_filmes = filmes.size();
-    cout << "Quantidade total de filmes: " << size_of_filmes << endl;
+    // cout << "Quantidade total de filmes: " << size_of_filmes << endl;
 
     long int todas_combinacoes = pow(2, size_of_filmes);
-    cout << "Quantidade total de combinações: " << todas_combinacoes << endl;
+    // cout << "Quantidade total de combinações: " << todas_combinacoes << endl;
     long int i;
 
-    #pragma omp parallel for
-    for (i = 0; i < todas_combinacoes; i++) {
+#pragma omp parallel for
+    for (i = 0; i < todas_combinacoes; i++)
+    {
         int num_films = 0;
         vector<int> categorias_vistas(categorias.size(), 0);
         stack<Filme> filmes_vistos;
@@ -73,9 +70,12 @@ void busca_exaustiva(vector<Filme>& filmes, vector<Categoria>& categorias, Marat
         bitset<64> filmes_vector(i);
         bitset<64> aux = filmes_vector;
 
-        for (int j = 0; j < size_of_filmes; j++) {
-            if (aux[j] == 1) {
-                if ((maratona_atual.disponibilidade & filmes[j].horario) == 0) {
+        for (int j = 0; j < size_of_filmes; j++)
+        {
+            if (aux[j] == 1)
+            {
+                if ((maratona_atual.disponibilidade & filmes[j].horario) == 0)
+                {
                     num_films++;
                     filmes_vistos.push(filmes[j]);
                     maratona_atual.disponibilidade |= filmes[j].horario;
@@ -83,17 +83,18 @@ void busca_exaustiva(vector<Filme>& filmes, vector<Categoria>& categorias, Marat
                 }
 
                 aux[j] = 0;
-
             }
         }
-        
-        #pragma omp critical
-        if (num_films > maximo) {
+
+#pragma omp critical
+        if (num_films > maximo)
+        {
             maximo = num_films;
             maratona.filmes.clear();
             maratona.disponibilidade.reset();
 
-            while (!filmes_vistos.empty()) {
+            while (!filmes_vistos.empty())
+            {
                 Filme filme = filmes_vistos.top();
                 filmes_vistos.pop();
                 maratona.filmes.push_back(filme);
@@ -102,37 +103,47 @@ void busca_exaustiva(vector<Filme>& filmes, vector<Categoria>& categorias, Marat
         }
     }
 
-    
+    // cout << "Quantidade máxima possível: " << maximo << endl;
+    cout << maximo << endl;
 
-    cout << "Quantidade máxima possível: " << maximo << endl;
-
-    for (int i = 0; i < maximo; i++) {
-        cout << "Filme: [" << maratona.filmes[i].id << "] " << maratona.filmes[i].inicio << " " << maratona.filmes[i].fim << " " << maratona.filmes[i].categoria << endl;
+    for (int i = 0; i < maximo; i++)
+    {
+        // cout << "Filme: [" << maratona.filmes[i].id << "] " << maratona.filmes[i].inicio << " " << maratona.filmes[i].fim << " " << maratona.filmes[i].categoria << endl;
+        cout << maratona.filmes[i].id << " " << maratona.filmes[i].inicio << " " << maratona.filmes[i].fim << " " << maratona.filmes[i].categoria << endl;
     }
 }
 
-int main() {
+int main()
+{
     int n, m;
     cin >> n >> m;
     vector<Filme> filmes;
     vector<Categoria> categorias(m);
     Maratona maratona;
-    
-    for (int i = 0; i < m; i++) {
+
+    for (int i = 0; i < m; i++)
+    {
         cin >> categorias[i].quantidade;
         categorias[i].id = i + 1;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         int inicio, fim, categoria;
         cin >> inicio >> fim >> categoria;
 
-        if (inicio > fim) {
-            if (fim == 0){
+        if (inicio > fim)
+        {
+            if (fim == 0)
+            {
                 fim = 24;
-            } else if (inicio == -1 || fim == -1) {
+            }
+            else if (inicio == -1 || fim == -1)
+            {
                 continue;
-            } else {
+            }
+            else
+            {
                 continue;
             }
         }
